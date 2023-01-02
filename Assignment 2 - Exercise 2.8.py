7e8f8cab2c9739281a69ae550ac52e1ea4d8157d
ianriccaboni <121349808+ianriccaboni@users.noreply.github.com>
2023-01-02 12:58:27 -0500

    Create Assignment 2 - Exercise 2.8.py

---
+++ Assignment 2 - Exercise 2.8.py
@@ -0,0 +1,4 @@
+pip install tabulate
+from tabulate install tabulate
+table = [['Number', 'Square', 'Cube'], [0, 0**2, 0**3], [1, 1**2, 1**3], [2, 2**2, 2**3], [3, 3**2, 3**3], [4, 4**2, 4**3], [5, 5**2, 5**3]]
+print(tabulate(table))
