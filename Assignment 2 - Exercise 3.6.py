fee712e27d150b524b684511e5b2472fd60d32a0
ianriccaboni <121349808+ianriccaboni@users.noreply.github.com>
2023-01-02 13:45:50 -0500

    Create Assignment 2 - Exercise 3.6.py

---
+++ Assignment 2 - Exercise 3.6.py
@@ -0,0 +1,7 @@
+input("What is your problem?")
+if 1:
+  print("Have you had this problem before (yes or no)?"):
+    if yes:
+      print("Well you have it again.")
+    if no:
+      print("Well you do now.")
