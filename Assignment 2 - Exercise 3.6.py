dd7f8c747c8d4e734ee8c9b11b88b1b098dba725
Ian Riccaboni <ianriccaboni@gmail.com>
2023-01-03 20:08:52 -0500

    Edit after solution review
    Reviewed solution, eliminated 1 function that was not working.

--- Assignment 2 - Exercise 3.6.py
+++ Assignment 2 - Exercise 3.6.py
@@ -1,7 +1,8 @@
-input("What is your problem?")
-if 1:
-  print("Have you had this problem before (yes or no)?"):
-    if yes:
-      print("Well you have it again.")
-    if no:
-      print("Well you do now.")
+input('What is your problem? ')
+
+response = input('Have you had this problem before (yes or no)? ')
+
+if response == 'yes':
+    print('Well, you have it again.')
+else:
+    print('Well, you have it now.')
