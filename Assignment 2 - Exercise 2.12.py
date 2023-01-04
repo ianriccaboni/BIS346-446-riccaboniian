7d802ef03ca10df63f3d69c59314e499801fa843
Ian Riccaboni <ianriccaboni@gmail.com>
2023-01-03 20:06:34 -0500

    Checked work
    Edited following checked work. Changed to simplified solution after trying circuitous code route.

--- Assignment 2 - Exercise 2.12.py
+++ Assignment 2 - Exercise 2.12.py
@@ -1,11 +1,5 @@
-p=1000
-r=.07
-n=10
-a=p*(1+r)**n
-a
-n=20
-a=p*(1+r)**n
-a
-a=p*(1+r)**n
-n=30
-a
+print('Balance following 10 years:', 1000 * (1 + .07) ** 10)
+
+print('Balance following 20 years:', 1000 * (1 + .07) ** 20)
+
+print('Balance following 30 years:', 1000 * (1 + .07) ** 30)
