a79989efce672dba55e168d361c859abdab2675b
ianriccaboni <121349808+ianriccaboni@users.noreply.github.com>
2023-01-20 14:41:50 -0500

    Final Exam Submission
    Final Exam Submission

---
+++ Ian Riccaboni - Final2022.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 14:24:20 2023
+
+@author: ianri
+"""
+
+import pandas as pd
+import pickle
+
+import matplotlib as mpl
+import matploblib.pyplot as plt
+
+plt.style.available[:20]
+plt.style.use('seaborn')
+
+YT = pd.read.csv('Bananas.csv')
+
+YT['Bananas price per lb'].describe().apply("{0:.2f}".format)
+YT['Date Sold']=pd.to_datetime(YT['DATE'], infer_datetime_format=True)
+
+plt.scatter(YT['Date Sold'],YT['Bananas price per lb'], label='Banana Prices in US', color='orange', marker="^")
+plt.ylabel('Price Per Pound, Bananas')
+plt.xlabel('Date Sold')
+plt.axis(1/1/2019, 12/1/2022, 0.56, 0.65)
+plt.legend(loc='upper right', shadow = 'True', fontsize = 'large' )
